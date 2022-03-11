caadeaf7ea6eb2700ac973a21efd4ce0df69f7a3
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-11 23:34:22 +0200

    Update listFavouriteCommand.ts

--- src/controllers/telegram/listFavouriteCommand.ts
+++ src/controllers/telegram/listFavouriteCommand.ts
@@ -5,32 +5,50 @@ import User from '../../models/user';
 import getCurrencyCoins from '../coin/getCurrencyCoins';
 
 const listFavouriteCommand = async (res, chat_id: number) => {
-  const { coin: userCoin } = await User.findOne({ chat_id });
+  const { coin: userCoin } = await User.findOne({ chatId: chat_id });
 
   let textMessage: string = `This is your list of favorite crypto coins and their average price per hour.`;
-  userCoin.forEach(async (symbol, i, arr) => {
-    const data: {
-      symbol: string;
-      price: string;
-    } = await getCurrencyCoins(symbol);
-    const price: string = Number(data.price).toFixed(2);
-    textMessage += `\n/${symbol} = ${price}$`;
+  if (userCoin.length > 0 || !userCoin) {
+    userCoin.forEach(async (symbol, i, arr) => {
+      const data: {
+        symbol: string;
+        price: string | any;
+      } = await getCurrencyCoins(symbol);
 
-    if (i === arr.length - 1) {
-      axios
-        .post(`${TELEGRAM_URL}/sendMessage`, {
-          chat_id,
-          text: textMessage,
-        })
-        .then(response => {
-          res.status(201).json({
-            status: 'success',
-            code: 201,
-          });
-        })
-        .catch(err => res.send(err));
-    }
-  });
+      const price: string | any = Number(data.price).toFixed(2);
+      textMessage += `\n/${symbol} = ${price}$`;
+
+      if (i === arr.length - 1) {
+        axios
+          .post(`${TELEGRAM_URL}/sendMessage`, {
+            chat_id,
+            text: textMessage,
+          })
+          .then(response => {
+            res.status(201).json({
+              status: 'success',
+              code: 201,
+            });
+          })
+          .catch(err => res.send(err));
+      }
+    });
+  } else {
+    textMessage += `\nsorry, but now you don't have your favorite coins\npress /listRecent select your favorite coin and add it to your favorites list`;
+
+    axios
+      .post(`${TELEGRAM_URL}/sendMessage`, {
+        chat_id,
+        text: textMessage,
+      })
+      .then(response => {
+        res.status(201).json({
+          status: 'success',
+          code: 201,
+        });
+      })
+      .catch(err => res.send(err));
+  }
 };
 
 export default listFavouriteCommand;
