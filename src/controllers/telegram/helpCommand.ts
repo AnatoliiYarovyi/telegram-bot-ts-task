4d05a74b5e75bcd154a1d0011a02b46a712fd950
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-14 18:37:26 +0200

    Update helpCommand.ts

--- src/controllers/telegram/helpCommand.ts
+++ src/controllers/telegram/helpCommand.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 const { TELEGRAM_URL } = process.env;
 
-const helpCommand = async (res, chat_id: number) => {
+import ResReqObj from '../interface/interface';
+
+const helpCommand = async (res: ResReqObj, chat_id: number) => {
   const text: string = `I am a cryptocurrency bot!\nI display the price of cryptocurrency! You can use commands like this:\n/listRecent --> get a list of popular cryptocurrencies;\n/{currency_symbol} --> get detailed information about cryptocurrency;\n/listFavourite --> returns a list of Favourite crypts;
   \nand you can adds or remove a crypt to the "favorites" section`;
 
