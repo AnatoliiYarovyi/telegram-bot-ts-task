dcfba6c99c82a5092e469836be199b082f55edd3
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:17:49 +0200

    Update getAllCoins.ts

--- src/controllers/coin/getAllCoins.ts
+++ src/controllers/coin/getAllCoins.ts
@@ -2,13 +2,16 @@ import axios from 'axios';
 const { SERVER_URL } = process.env;
 
 const getAllCoins = async () => {
-  return axios
-    .get(SERVER_URL)
-    .then(res => {
+  try {
+    const resData = await axios.get(SERVER_URL).then(res => {
       const data = res.data.data;
       return data;
-    })
-    .catch(err => console.log(err));
+    });
+
+    return resData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getAllCoins;
