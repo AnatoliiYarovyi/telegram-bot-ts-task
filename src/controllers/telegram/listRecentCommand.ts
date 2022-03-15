280eae0aa6ae29d4368534edf6b12362cfc6cd47
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:18:20 +0200

    Update listRecentCommand.ts

--- src/controllers/telegram/listRecentCommand.ts
+++ src/controllers/telegram/listRecentCommand.ts
@@ -5,27 +5,30 @@ import getAllCoins from '../coin/getAllCoins';
 import ResReqObj from '../interface/interface';
 
 const listRecentCommand = async (res: ResReqObj, chat_id: number) => {
-  const data: {
-    symbol: string;
-    price_average: string;
-  }[] = await getAllCoins();
-  let text: string = `These are 20 popular crypto coins and their average price per hour.`;
-  await data.forEach(({ symbol, price_average }) => {
-    let price = Number(price_average).toFixed(2);
-    text += `\n/${symbol} = ${price}$`;
-  });
-  axios
-    .post(`${TELEGRAM_URL}/sendMessage`, {
-      chat_id,
-      text,
-    })
-    .then(response => {
-      res.status(201).json({
-        status: 'success',
-        code: 201,
+  try {
+    const data: {
+      symbol: string;
+      price_average: string;
+    }[] = await getAllCoins();
+    let text: string = `These are 20 popular crypto coins and their average price per hour.`;
+    await data.forEach(({ symbol, price_average }) => {
+      let price = Number(price_average).toFixed(2);
+      text += `\n/${symbol} = ${price}$`;
+    });
+    await axios
+      .post(`${TELEGRAM_URL}/sendMessage`, {
+        chat_id,
+        text,
+      })
+      .then(() => {
+        res.status(201).json({
+          status: 'success',
+          code: 201,
+        });
       });
-    })
-    .catch(err => res.send(err));
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 export default listRecentCommand;
