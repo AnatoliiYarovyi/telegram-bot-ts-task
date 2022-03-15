c62002ed2a7d7da1ff1882ba89300b63ba4c9b72
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:18:08 +0200

    Update helpCommand.ts

--- src/controllers/telegram/helpCommand.ts
+++ src/controllers/telegram/helpCommand.ts
@@ -4,21 +4,24 @@ const { TELEGRAM_URL } = process.env;
 import ResReqObj from '../interface/interface';
 
 const helpCommand = async (res: ResReqObj, chat_id: number) => {
-  const text: string = `I am a cryptocurrency bot!\nI display the price of cryptocurrency! You can use commands like this:\n/listRecent --> get a list of popular cryptocurrencies;\n/{currency_symbol} --> get detailed information about cryptocurrency;\n/listFavourite --> returns a list of Favourite crypts;
+  try {
+    const text: string = `I am a cryptocurrency bot!\nI display the price of cryptocurrency! You can use commands like this:\n/listRecent --> get a list of popular cryptocurrencies;\n/{currency_symbol} --> get detailed information about cryptocurrency;\n/listFavourite --> returns a list of Favourite crypts;
   \nand you can adds or remove a crypt to the "favorites" section`;
 
-  axios
-    .post(`${TELEGRAM_URL}/sendMessage`, {
-      chat_id,
-      text,
-    })
-    .then(response => {
-      res.status(201).json({
-        status: 'success',
-        code: 201,
+    await axios
+      .post(`${TELEGRAM_URL}/sendMessage`, {
+        chat_id,
+        text,
+      })
+      .then(() => {
+        res.status(201).json({
+          status: 'success',
+          code: 201,
+        });
       });
-    })
-    .catch(err => res.send(err));
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 export default helpCommand;
