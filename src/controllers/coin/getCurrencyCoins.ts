f295b6dc20f724a74da2e2844a7732cf7a65184b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:17:57 +0200

    Update getCurrencyCoins.ts

--- src/controllers/coin/getCurrencyCoins.ts
+++ src/controllers/coin/getCurrencyCoins.ts
@@ -2,15 +2,19 @@ import axios from 'axios';
 const { SERVER_COIN_URL } = process.env;
 
 const getCurrencyCoins = async (symbol: string, time: number = 60) => {
-  return axios
-    .get(SERVER_COIN_URL, {
-      params: { name: symbol, timeInMinutes: time },
-    })
-    .then(res => {
-      const data = res.data.data;
-      return data;
-    })
-    .catch(err => console.log(err));
+  try {
+    const resData = await axios
+      .get(SERVER_COIN_URL, {
+        params: { name: symbol, timeInMinutes: time },
+      })
+      .then(res => {
+        const data = res.data.data;
+        return data;
+      });
+    return resData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getCurrencyCoins;
