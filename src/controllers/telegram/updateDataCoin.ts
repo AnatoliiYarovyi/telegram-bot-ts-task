026eb54355de882c1b0b6ffaf331054714982552
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-15 14:18:34 +0200

    Update updateDataCoin.ts

--- src/controllers/telegram/updateDataCoin.ts
+++ src/controllers/telegram/updateDataCoin.ts
@@ -18,35 +18,38 @@ const updateDataCoin: UpdateDataCoin = async (
   callback_query_id,
   message_id,
 ) => {
-  const { coin: userCoin } = await User.findOne({ chatId: chat_id });
-  userCoin.includes(symbol)
-    ? userCoin.splice(userCoin.indexOf(symbol), 1)
-    : userCoin.push(symbol);
+  try {
+    const { coin: userCoin } = await User.findOne({ chatId: chat_id });
+    userCoin.includes(symbol)
+      ? userCoin.splice(userCoin.indexOf(symbol), 1)
+      : userCoin.push(symbol);
 
-  await User.findOneAndUpdate(
-    { chatId: chat_id },
-    { $set: { coin: userCoin } },
-  );
+    await User.findOneAndUpdate(
+      { chatId: chat_id },
+      { $set: { coin: userCoin } },
+    );
 
-  let text: string = userCoin.includes(symbol)
-    ? `/${symbol} add to favorite`
-    : `/${symbol} remove from favorite`;
+    let text: string = userCoin.includes(symbol)
+      ? `/${symbol} add to favorite`
+      : `/${symbol} remove from favorite`;
 
-  axios.post(`${TELEGRAM_URL}/answerCallbackQuery`, {
-    callback_query_id,
-    text,
-  });
-
-  axios
-    .post(`${TELEGRAM_URL}/editMessageText`, {
-      chat_id,
-      message_id,
+    await axios.post(`${TELEGRAM_URL}/answerCallbackQuery`, {
+      callback_query_id,
       text,
-    })
-    .then(response => {
-      res.status(201).send(response);
-    })
-    .catch(err => res.send(err));
+    });
+
+    await axios
+      .post(`${TELEGRAM_URL}/editMessageText`, {
+        chat_id,
+        message_id,
+        text,
+      })
+      .then(response => {
+        res.status(201).send(response);
+      });
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 export default updateDataCoin;
