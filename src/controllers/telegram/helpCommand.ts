2134a906ef053f6e44c3ed22bb3a8ffa4c907dbd
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-16 02:02:59 +0200

    Update helpCommand.ts

--- src/controllers/telegram/helpCommand.ts
+++ src/controllers/telegram/helpCommand.ts
@@ -5,8 +5,7 @@ import ResReqObj from '../interface/interface';
 
 const helpCommand = async (res: ResReqObj, chat_id: number) => {
   try {
-    const text: string = `I am a cryptocurrency bot!\nI display the price of cryptocurrency! You can use commands like this:\n/listRecent --> get a list of popular cryptocurrencies;\n/{currency_symbol} --> get detailed information about cryptocurrency;\n/listFavourite --> returns a list of Favourite crypts;
-  \nand you can adds or remove a crypt to the "favorites" section`;
+    const text: string = `I am a cryptocurrency bot!\nI display the price of cryptocurrency!\nYou can use commands like this:\n/listRecent --> get a list of popular cryptocurrencies;\n/{currency_symbol} --> get detailed information about cryptocurrency;\n/listFavorite --> return a list of "favorite" crypts;\n/addToFavorite {currency_symbol} --> add a crypt to the "favorite";\n/deleteFavorite {currency_symbol} --> remove crypto from "favorite";`;
 
     await axios
       .post(`${TELEGRAM_URL}/sendMessage`, {
