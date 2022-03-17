95e02d1501b5b1eb5a494641c1e17f415502bd78
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-03-17 15:46:54 +0200

    Update main.ts

--- src/controllers/telegram/main.ts
+++ src/controllers/telegram/main.ts
@@ -10,60 +10,65 @@ import deleteFavoriteCommand from './deleteFavoriteCommand';
 
 import ResReqObj from '../interface/interface';
 
-const main = async (req: ResReqObj, res: ResReqObj, next: void) => {
-  if (!req.body.callback_query) {
-    const { id: chatId, username } = req.body.message.chat;
-    const { text } = req.body.message;
-    const textArr: string = text.replace(/\s+/g, ' ').trim().split(' ');
-    const command: string = textArr[0];
-    const symbolCoin: string = textArr[1];
+const main = async (req: ResReqObj, res: ResReqObj, next) => {
+  try {
+    if (!req.body.callback_query) {
+      const { id: chatId, username } = req.body.message.chat;
+      const { text } = req.body.message;
+      const textArr: string = text.replace(/\s+/g, ' ').trim().split(' ');
+      const command: string = textArr[0];
+      const symbolCoin: string = textArr[1];
 
-    switch (command) {
-      case '/start':
-        await startCommand(res, chatId, username);
-        break;
+      switch (command) {
+        case '/start':
+          await startCommand(res, chatId, username);
+          break;
 
-      case '/help':
-        await helpCommand(res, chatId);
-        break;
+        case '/help':
+          await helpCommand(res, chatId);
+          break;
 
-      case '/listRecent':
-        await listRecentCommand(res, chatId);
-        break;
+        case '/listRecent':
+          await listRecentCommand(res, chatId);
+          break;
 
-      case `/listFavorite`:
-        await listFavouriteCommand(res, chatId);
-        break;
+        case `/listFavorite`:
+          await listFavouriteCommand(res, chatId);
+          break;
 
-      case `/addToFavorite`:
-        await addToFavoriteCommand(res, chatId, symbolCoin);
-        break;
+        case `/addToFavorite`:
+          await addToFavoriteCommand(res, chatId, symbolCoin);
+          break;
 
-      case `/deleteFavorite`:
-        await deleteFavoriteCommand(res, chatId, symbolCoin);
-        break;
+        case `/deleteFavorite`:
+          await deleteFavoriteCommand(res, chatId, symbolCoin);
+          break;
 
-      case `/${await getSymbolCoin(text)}`:
-        await currencySymbolCommand(res, chatId, text);
-        break;
+        case `/${await getSymbolCoin(text)}`:
+          await currencySymbolCommand(res, chatId, text);
+          break;
 
-      default:
-        console.log('Invalid command');
-        await helpCommand(res, chatId);
+        default:
+          console.log('Invalid command');
+          await helpCommand(res, chatId);
+      }
+    } else if (req.body.callback_query) {
+      const { id: chatId } = req.body.callback_query.from;
+      const symbolCoin: string = req.body.callback_query.data;
+      const callback_query_id: string = req.body.callback_query.id;
+      const message_id: number = req.body.callback_query.message.message_id;
+
+      await updateDataCoin(
+        res,
+        chatId,
+        symbolCoin,
+        message_id,
+        callback_query_id,
+      );
     }
-  } else if (req.body.callback_query) {
-    const { id: chatId } = req.body.callback_query.from;
-    const symbolCoin: string = req.body.callback_query.data;
-    const callback_query_id: string = req.body.callback_query.id;
-    const message_id: number = req.body.callback_query.message.message_id;
-
-    await updateDataCoin(
-      res,
-      chatId,
-      symbolCoin,
-      message_id,
-      callback_query_id,
-    );
+  } catch (error) {    
+    console.log(error.message);
+    next(error);
   }
 };
 
